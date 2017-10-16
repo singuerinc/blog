411ac6a21c8a3f4f45ec728beb4179beb0a5647b
singuerinc <nahuel.scotti@gmail.com>
2017-10-16 23:24:00 +0200

    remove everything

--- code/labs/require-marionette-movies/templates/helpers/popularityWidth.js
+++
@@ -1,8 +0,0 @@
-define(['hbs/handlebars'], function (Handlebars) {
-
-  Handlebars.registerHelper('popularityWidth', function (popularity, options) {
-
-    return "width:" + Math.floor(Math.min(1, popularity) * 98) + "px;"
-
-  });
-});
