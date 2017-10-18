bfe3803288b5e28a9d68a2724c5e2d458d9e523f
singuerinc <nahuel.scotti@gmail.com>
2017-10-18 23:21:15 +0200

    feat: add tachyons

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,56 +3,31 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import './index.css'
+import 'tachyons'
 import 'prismjs/themes/prism-tomorrow.css'
+import './index.css'
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
+  <div>
+    <div>
+      <h1>
+        <Link to="/">@singuerinc</Link>
       </h1>
     </div>
   </div>
 )
 
 const TemplateWrapper = ({ children }) => (
-  <div>
+  <div className="sans-serif near-black">
     <Helmet
-      title="Gatsby Default Starter"
+      title="singuerinc | blog"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div>
       {children()}
     </div>
   </div>
